c103cedc6e3c5e2555ce81fae8546017f0c0d925
bobkings <robertoc-95@hotmail.com>
2024-03-30 14:50:27 -0600

    se corrige router de user

--- src/router/user.router.js
+++ src/router/user.router.js
@@ -7,7 +7,7 @@ router.get('/prueba-user', check.auth(), UserController.prueba);
 router.post('/register', check.auth('0'), UserController.register);
 router.put('/update/:id', check.auth('0'), UserController.update);
 router.get('/list/:page?', check.auth('0'), UserController.list);
-router.get('/list-one/:id', check.auth('0'), UserController.listOne);
+router.get('/list-one/:id', check.auth(), UserController.listOne);
 router.post("/login", UserController.login);
 router.delete('/delete/:id', check.auth('0'), UserController.deleteUser);
 
